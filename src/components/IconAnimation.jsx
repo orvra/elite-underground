252b3c4b19ce88d09fc4346712ae3d3ad7bd8b85
orvra <135913026+orvra@users.noreply.github.com>
2023-09-03 19:53:45 -0400

    eug v4
    fix?

--- src/components/IconAnimation.jsx
+++ src/components/IconAnimation.jsx
@@ -23,7 +23,7 @@ const IconAnimation = ({ interval }) => {
     <MdSportsTennis />,
     <MdSportsGolf />,
     <img
-      src="/src/images/eug_v8.png"
+      src="../src/images/eug_v8.png"
       className="w-[83.3px] pt-1 md:w-[108px] md:pt-[5.7px] lg:w-[188px] lg:pt-[10px]"
     />,
   ];
