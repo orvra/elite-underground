abcc6078eb0ba92789fc39f7b2b257b1e2c98ca5
orvra <135913026+orvra@users.noreply.github.com>
2023-09-03 21:59:57 -0400

    eug v4
    image problem

--- src/data/products.js
+++ src/data/products.js
@@ -8,7 +8,7 @@ gender: 'Women',
 category: 'sports bras tops',
 color: 'Black',
 price: 40,
-src: '/src/images/clothing/black_sports_bra.jpg',
+src: 'elite-underground/src/images/clothing/black_sports_bra.jpg',
 rating: 4.5,
 fit: 30
 },
